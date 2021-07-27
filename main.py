22eba93d3fe39a2b2cd7d264f73170ffcb3b1c69
Robert_Norwood <robnorwood91@gmail.com>
2021-07-26 20:07:32 -0700

    Created the basic calculation formula and saved to a variable.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #Welcome the user to the program
 opening_message = "Welcome to the tip calculator!"
-print(opening_message)
+
 #Get the total bill amount.
 #Convert the bill to a float number by wrapping the input with "float".
 total_bill = float(input('What was the total bill for tonight? $' ))
-print(total_bill)
+
 #Create a variable for the tip, and convert that to an integer. 
 tip_percentage = int(input('What percentage of a tip would you like to leave? 5, 10, 12, or 15? '))
-print(tip_percentage)
+
+#Create a variable for the amount of people splitting bill, then convert it to an integer.
+people_to_split_bill = int(input('How many people are splitting the bill? '))
+
+#Divide the tip percentage by 100, then multipy that by the total bill amount, and lastly add that to the total bill. 
+bill_with_tip = tip_percentage / 100 * total_bill + total_bill
+print(bill_with_tip)
